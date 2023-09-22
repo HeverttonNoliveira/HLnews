d744fe844110355a78433c6a24f0114ffb336f21
LucasGonzagaCode <lucasgonzagameireles@gmail.com>
2023-09-22 00:16:05 -0300

    commit-2

--- js/js.js
+++ js/js.js
@@ -1,1 +1,8 @@
-alert ("Hellow")
+let form = document.querySelector('.formulario')
+
+const OpenForm = () => {
+    form.classList.add('active')
+}
+const CloseForm = () => {
+ form.classList.remove('active')
+}
